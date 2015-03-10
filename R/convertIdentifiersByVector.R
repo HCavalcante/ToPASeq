6771787939f796926c68f4ffde335d5d7a1600de
i.ihnatova <i.ihnatova@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-03-10 14:24:05 +0000

    Adding manipulation and basic topology analysis
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ToPASeq@100461 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/convertIdentifiersByVector.R
+++ R/convertIdentifiersByVector.R
@@ -1,13 +1,16 @@
 #conv.table = named vector of new identifiers, names refer to the original ones
 #id.type = type of the new identifiers  e.g. "TAIR"
 
-convertIdentifiersByVector<-function (pathway, conv.table, id.type) 
+convertIdentifiersByVector<-function (pathway, conv.table, id.type="unknown") 
 {
     ns <- nodes(pathway)
     es <- edges(pathway)
     if (!all(ns %in% names(conv.table))) 
-      stop(paste("These pathway nodes are missing in the 'conv.table':",ns[! ns %in% names(conv.table)]))
+      warning(paste("These pathway nodes are missing in the 'conv.table':", paste(ns[! ns %in% names(conv.table)], collapse=", "),". The original identifiers were kept."))
 
+
+    miss <- ns[! ns %in% names(conv.table)]
+    conv.table<-c(conv.table, setNames(miss,miss))
     ns <- unname(conv.table[nodes(pathway)])
     es[,1]<- unname(conv.table[es[,1]])
     es[,2]<- unname(conv.table[es[,2]])
--- R/isComplex.R
+++ R/isComplex.R
@@ -1,11 +1,13 @@
-isComplex<-function(g, x){
-e<-edges(g)
-sube<-e[e[,1] %in% x & e[,2] %in% x,]
-nrow(sube)==length(x) &
-all(x %in% unlist(sube[,1:2])) &
-all(sube[,3]=="undirected") &
-all(regexpr("binding", sube[,4])!=-1) 
+isComplex<-function (graph, x) 
+{   
+    
+    e <- edges(graph)
+    sube <- e[e[, 1] %in% x & e[, 2] %in% x, ]
+    nrow(sube) == ncol(combn(x,2)) & all(x %in% unlist(sube[, 1:2])) & 
+        all(sube[, 3] == "undirected") & 
+        all( (regexpr("binding",sube[, 4]) != -1) )
 }
+
 #example
 #g<-convertIdentifiers(kegg[["p53 signaling pathway"]], "symbol")
 #x<-c("CCNE1", "CDK2",  "CCNE2")
