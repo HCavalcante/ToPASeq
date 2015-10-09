26eba800cf106d4a9363d0e10d33c3d45dc0ddd2
i.ihnatova <i.ihnatova@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-10-09 19:15:37 +0000

    fixing edge type conversion table
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ToPASeq@109439 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makeDefaultEdgeData.r
+++ R/makeDefaultEdgeData.r
@@ -1,19 +1,16 @@
 makeDefaultEdgeData<-function(){
 graphite2SPIA<-data.frame(
- type=c("binding", "control(In(ACTIVATION))", "control(In(INHIBITION))",
-   "control(Out(ACTIVATION))", "control(Out(INHIBITION))", 
-   "control(Out(INHIBITION-COMPETITIVE))", "control(Out(ACTIVATION_UNKMECH))",
-   "control(Out(unknown))", "control(indirect)", "process", "process(BiochemicalReaction)",
-   "process(activation)",  "process(binding/association)", "process(dephosphorylation)",
-   "process(dissociation)", "process(expression)", "process(indirect effect)", 
-   "process(indirect)", "process(inhibition)", "process(missing interaction)",
-   "process(missing)", "process(phosphorylation)", "process(repression)",
-   "process(ubiquitination)", "process(methylation)", "process(state change)")
- ,
- spiaType=c("binding/association", "activation", "inhibition", "activation", "inhibition", "inhibition", "activation", 
-"indirect effect", "indirect effect", "activation", "activation", "activation", "binding/association", 
-"dephosphorylation", "dissociation", "expression", "indirect effect", "indirect effect", "inhibition", 
-"indirect effect", "indirect effect", "phosphorylation", "inhibition", "ubiquination", "inhibition", "ubiquination") 
+type=c("Binding", "Control(In: ACTIVATION of BiochemicalReaction)", "Control(In: ACTIVATION of ComplexAssembly)",
+  "Control(In: ACTIVATION of Degradation)", "Control(In: ACTIVATION of Transport)", "Control(indirect)",           
+  "Control(In: INHIBITION-COMPETITIVE of BiochemicalReaction)", "Control(In: INHIBITION of BiochemicalReaction)",
+  "Control(In: INHIBITION of ComplexAssembly)", "Control(In: INHIBITION of Transport)", "Control(Out: ACTIVATION of ACTIVATION)",
+  "Control(Out: ACTIVATION of BiochemicalReaction)", "Control(Out: ACTIVATION of ComplexAssembly)", "Control(Out: ACTIVATION of TemplateReaction)", "Control(Out: ACTIVATION of Transport)", "Control(Out: INHIBITION-COMPETITIVE of BiochemicalReaction)", "Control(Out: INHIBITION of ACTIVATION)", "Control(Out: INHIBITION of BiochemicalReaction)", "Control(Out: INHIBITION of ComplexAssembly)", 
+  "Control(Out: INHIBITION of TemplateReaction)", "Control(Out: INHIBITION of Transport)", "Process", "Process(activation)", "Process(binding/association)", "Process(BiochemicalReaction)", "Process(dephosphorylation)", "Process(dissociation)", "Process(expression)", 
+  "Process(glycosylation)", "Process(indirect)", "Process(indirect effect)", "Process(inhibition)", "Process(methylation)", "Process(missing)", "Process(missing interaction)", "Process(phosphorylation)", "Process(repression)", "Process(state change)", "Process(ubiquitination)"),
+spiaType=c("binding/association", "activation", "activation", "activation", "activation", "indirect effect", "inhibition", "inhibition", "inhibition", "inhibition", "activation", "activation", "activation", "activation", "activation", "inhibition", "inhibition",
+"inhibition", "inhibition", "inhibition", "inhibition", "activation", "activation", "binding/association", "activation",
+"dephosphorylation", "dissociation", "expression", "glycosylation", "indirect effect", "indirect effect", "inhibition", "methylation",
+"indirect effect", "indirect effect", "phosphorylation", "inhibition", "indirect effect",  "ubiquitination")
  , stringsAsFactors=FALSE)
  graphite2SPIA<-as.matrix(graphite2SPIA)
  beta<-data.frame(
